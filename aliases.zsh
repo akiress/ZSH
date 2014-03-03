1790ed8a78be79ecab7a03a3db4fc5dda0630b76
Ben Guitreau <akiress@thebeyond.(none)>
2014-03-03 12:55:37 -0600

    Adding zsh back to github page

---
+++ aliases.zsh
@@ -0,0 +1,16 @@
+# List directory contents
+export COLOR='--color=auto'
+alias lsa='ls $COLOR -lahrt'
+alias l='ls $COLOR -lahrt'
+alias ll='ls $COLOR -lahrt'
+alias ls='ls $COLOR -lahrt'
+alias dir='ls $COLOR -lahrt'
+
+# Hub
+alias git='hub'
+
+# Elogv
+alias elogv='/usr/bin/elogv-python2.7'
+
+# Mutt
+alias mutt='mutt -y'
---
+++ git.plugin.zsh
@@ -0,0 +1,22 @@
+function current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
+
+function current_repository() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  echo $(git remote -v | cut -d':' -f 2)
+}
+
+function _git_log_prettily(){
+  if ! [ -z $1 ]; then
+    git log --pretty=$1
+fi
+}
+
+function git_prompt_info() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$ZSH_THEME_GIT_PROMPT_SUFFIX"
+}
